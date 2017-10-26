52d44903b24d7864c6697973c481485433892fc0
beneva86 <benikovszky.eva@gmail.com>
2017-10-26 21:11:59 +0200

    redirect to users page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // GET homepage
 app.get('/', function(request, response) {
-	response.render('index');
+    response.render('index');
 });
 
 //GET all users
@@ -25,16 +25,16 @@ app.get('/users', function(request, response){
         }
 
         var parsedData = JSON.parse(data);
-        console.log("This is the app.get(/users) request");
-        response.render('./users', {users: parsedData})
+        // console.log("This is the app.get(/users) request");
+        response.render('users', {users: parsedData})
 
-	})
+    })
 })
 
 //GET search users
 app.get('/search', function(request,response){
-	var input = request.query.q;
-	fs.readFile('./users.json', function (error, data) { // callback with (parameters)
+    var input = request.query.q;
+    fs.readFile('./users.json', function (error, data) { // callback with (parameters)
         if (error) {
             throw error;
         }
@@ -42,59 +42,58 @@ app.get('/search', function(request,response){
         var parsedData = JSON.parse(data);
         console.log(parsedData);
 
-		var output = parsedData.find(function(element) {
-		return element.firstname.toLowerCase() === input || element.lastname.toLowerCase() === input; 
-		});
-		response.render('./search', {matchedUsers: output})
-	});
+        var output = parsedData.find(function(element) {
+        return element.firstname.toLowerCase() === input || element.lastname.toLowerCase() === input; 
+        });
+        response.render('search', {matchedUsers: output})
+    });
 
-	// var output = parsedData.find(element => element.firstname === input || element.lastname === input);
-	
+    // var output = parsedData.find(element => element.firstname === input || element.lastname === input);
+    
 })
 
 // GET new users
 app.get('/new', function(request, response) {
-	response.render('new');
+    response.render('new');
 });
 
 // POST new users
 
 app.post('/new', function(request,response){
-	var firstName = request.body.firstname.toLowerCase();
-	var lastName = request.body.lastname.toLowerCase();
-	var email = request.body.email.toLowerCase();
 
+    var firstName = request.body.firstname.toLowerCase();
+    var lastName = request.body.lastname.toLowerCase();
+    var email = request.body.email.toLowerCase();
 
-
-	fs.readFile('./users.json', function (error, data) { // callback with (parameters)
+    fs.readFile('./users.json', function (error, data) { // callback with (parameters)
         if (error) {
             throw error;
         }
 
         var parsedData = JSON.parse(data);
         var newUser = {
-        	firstname: firstName,
-        	lastname: lastName, 
-        	email: email,
+            firstname: firstName,
+            lastname: lastName, 
+            email: email,
         }
 
         parsedData.push(newUser);
 
-        var newJson = JSON.stringify(parsedData)
+        // var newJson = JSON.stringify(parsedData)
         // console.log(newJson);
         console.log(parsedData)
 
       
-      fs.writeFile('./users.json', newJson, function(err){
-      	if (err){
-      		response.send('error: ' + err)
-      	}
-
+      fs.writeFile('./users.json', JSON.stringify(parsedData), function(err){
+        if (err){
+            response.send('error: ' + err)
+        }
       })
-        response.send('/users', {users: parsedData})
-	
-})
-})	
+
+       response.redirect('/users') 
+    
+    })
+})  
 
 app.listen(3000, function() {
     console.log('User app listening on port 3000!')
