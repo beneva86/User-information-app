6f1201a9e7d7b0eef8e3c8cf2d1242f64f96353a
beneva86 <benikovszky.eva@gmail.com>
2017-11-13 17:21:41 +0100

    using ajax request

--- public/js/users.js
+++ public/js/users.js
@@ -1,43 +1,34 @@
+$(".matches").hide();
 $(document).ready(function() {
-  init();
+
+  let list = [];
+
+$.get('/search', function(response) {
+  list = response.list  
+//  console.log(list)   // it works
 });
 
+  $(".username").keyup(function() {  
+      let matchedUsers = "";    
+      let input = $(".username").val().toLowerCase(); 
+      // console.log(input)   // it works
+      if (input != "") {
 
-function init() {
-
-  var isOpened = false;
-  var matchCount = 0;
-
-  $(".searchbar input").on("input", function() {  // ha input erkezik az input mezobe
-    var criteria = $(this).val(); // a criteria var legyen a begepelt input
-    if (criteria !== "") {    // ha az input mezo nem ures
-      $.get('/search', {input:criteria}, function(users) { // a /users route-n erkezo JSON file olvasasa
-        $(".matches").empty();  // a div.matches legyen ures
-
-        $.each(users, function() {  
-          var fullname = this.firstname + " " + this.lastname;  // var fullname legyen a teljes nev
-          console.log(fullname)
-          if (fullname.toLowerCase().indexOf(criteria.toLowerCase()) > -1) {  // ha a begepelt szoveg barhol is tartalmazza a begepelt szoveget
-            $('.matches').html('<div>' + fullname + '</div>');  // akkor a div.matchesben jelenleg meg az a fullname,ami tartalmazza a begepelt szoveget
-            matchCount++; // ez az .each function miatt kell,hogy megismetelje a vizsgalatot (iteration)
-          }
+          const fullname = list.filter(function (element) {
+             return element.toLowerCase().includes(input);
         });
-        if (matchCount > 0 && !isOpened) {
-            $(".matches").addClass('open');
-            isOpened = true;  // hol van az isOpen variable meghatarozva? Ha csak eliras es isOpened lenne, akkor miert kell true-ra atirni az erteket, mikor 2sorral feljebb az a feltetelunk,hogy
-        }
-        matchCount = 0;
-      });
-    } else {
-      isOpened = false; 
-      $(".matches").removeClass('open'); // ha az input mezo ures, akkor a div zarodjon be
-      // $(".matches").empty(); // ez miert kell? miert mondjuk,hogy legyen ures, ha elotte azt mondjuk,hogy zarodjon be a div?
-    }
-  });
+        // console.log(fullname)  // it is an array with the matched usernames        
+            for(i=0; i<fullname.length; i++){
+              matchedUsers += `<li class="listItem" name="${matchedUsers[i]}">${fullname[i]}</li>`
+            }
 
-  $('body').on('click', '.matches > div', function() {
-    $('.username ').val($(this).text()).focus();
-    $(".matches").removeClass('open');
-  });
+            $('.listofmatches').html(`${matchedUsers}`);
+        } 
 
-};
+        $('.listItem').on('click', function() {
+           $('.username').val($(this).text()).focus();
+           $('.matches').addClass('close');
+      })
+  });
+});  
+
